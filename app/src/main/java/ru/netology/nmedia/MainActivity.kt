061d0f0e84367c259d07a7c7ad12a27df099af25
Alexander <9922202@gmail.com>
2022-04-26 14:33:51 +0300

    Homework4 events Initial commit

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,41 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.databinding.PostBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.post) {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = PostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1L,
+            author = "Alexander",
+            content = "Hello! It is a project by Netology",
+            published = "26.04.2022"
+        )
+
+        with(binding) {
+            usersName.text = post.author
+            date.text = post.published
+            mainText.text = post.content
+            if(post.likedByMe) {
+                likesImage.setImageResource(R.drawable.ic_liked_24dp)
+
+                likesImage.setOnClickListener {
+                    post.likedByMe = !post.likedByMe
+                    likesImage.setImageResource (
+                        if (post.likedByMe) R.drawable.ic_liked_24dp else R.drawable.ic_likes_24dp
+                    )
+
+                }
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia
+
+class Post (
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likedByMe: Boolean = false,
+        ) {
+
+}
