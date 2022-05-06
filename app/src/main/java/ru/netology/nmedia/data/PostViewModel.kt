29eddb1b104345c9c50cac62acce2b1bbbde4efa
Alexander <9922202@gmail.com>
2022-05-06 10:29:48 +0300

    Intermediate commit

--- app/src/main/java/ru/netology/nmedia/data/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/data/PostViewModel.kt
@@ -1,11 +1,12 @@
 package ru.netology.nmedia.data
 
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.Post
 
 class PostViewModel: ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.get()
-    fun like() = repository.like()
-    fun share() = repository.share()
+    fun like(id: Long) = repository.like(id)
+    fun share(id: Long) = repository.share(id)
 
 }
